1fde7c66fcd1b0848a99ce55cc43045643c32538
lehungio <me@lehungio.com>
2024-11-02 19:31:13 +0900

    namespace

--- src/main/scala/com/lehungio/Challenges.scala
+++ src/main/scala/com/lehungio/Challenges.scala
@@ -2,7 +2,7 @@ package com.lehungio
 
 import com.lehungio.classes.HelloWorld
 
-object ScalaChallenges {
+object Challenges {
   def main(args: Array[String]): Unit = {
     // Creating an instance of HelloWorld will output the challenge name
     val helloWorld = new HelloWorld()
